83139ed3c82e88d33f8998eb11e03c7ee10110b8
Daniel Oliveira <danielsapup3@gmail.com>
2023-11-09 09:19:29 -0400

    CSS Global

---
+++ 4 - Seção - React e CSS/react-css_51/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+
+// ------- Principal -------
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1>CSS Global</h1>
+        <hr />
+      </div>
+    </>
+  )
+}
+
+export default App
