330d059b5a0366f0ac9523d5fb518842143fd5ef
Daniel Oliveira <danielsapup3@gmail.com>
2023-11-12 09:25:56 -0400

    Resetando formulários

--- 5 - Seção - Formulários com React/README.md
+++ 5 - Seção - Formulários com React/README.md
@@ -128,10 +128,16 @@ npm run dev
 
 
 
+### 65 - Resetando formulários
 
+**Limpando formulários**
 
+- Com o controller inputs limpar o form será **fácil**
+- Basta **atribuir um valor de uma string vazia aos states** e pronto
+- Isso será feito após o envio, em formulários que o usuário precisa preencher novamente
 
-
+- Exemplo:
+    - react-form_65
 
 
 
---
+++ 5 - Seção - Formulários com React/react-form_65/src/components/MyForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import './MyForm.css';
+
+// ------- Component -------
+const MyForm = ({ user }) => {
+  // 6 - Controlled inputs
+
+
+  // 3 - Gerenciamento de dados
+  const [nome, setNome] = useState(user ? user.nome : '');
+  const [email, setEmail] = useState(user ? user.email : '');
+
+  // função que altera o valor do nome
+  const handleNome = (e) => {
+    setNome(e.target.value);
+  };
+
+  // função de envio
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Enviando o formulário!");
+    console.log(nome);
+    console.log(email);
+
+    // validação
+    // envio
+
+    // 7 - limpar form
+    setNome('');
+    setEmail('');
+  };
+
+  return (
+    <>
+        <div className="container-title">
+            <h1>Envio de form</h1>
+            <p>Meu formulário</p>
+            <hr />
+        </div>
+        {/*5 - Envio do Formulário */}
+        {/*1 - Criação do Formulário */}
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                  <span>Nome</span>
+                  <input type="text" id="nome" placeholder="Digite o seu nome" onChange={handleNome} value={nome} />
+                </label>
+                
+                <label>
+                  <span>Email</span>
+                  <input type="email" id="email" placeholder="Digite o seu Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                </label>
+            </div>
+                <input type="submit" value="Enviar" />
+        </form>
+    </>
+  )
+}
+
+export default MyForm;
