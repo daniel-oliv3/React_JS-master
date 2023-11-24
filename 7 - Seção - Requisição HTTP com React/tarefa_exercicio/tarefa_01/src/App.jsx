e9e3cb17c504fad40fd583683e78dec163db6296
Daniel Oliveira <danielsapup3@gmail.com>
2023-11-24 09:15:30 -0400

    Update

--- 7 - Seção - Requisição HTTP com React/tarefa_exercicio/tarefa_01/src/App.jsx
+++ 7 - Seção - Requisição HTTP com React/tarefa_exercicio/tarefa_01/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// CSS
+import './App.css';
 
+// React
+import { useState, useEffect } from 'react';
+
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
+const url = "http://localhost:3000/products";
+// const url = "http://localhost:3001/products"; // simulando error
+
+
+// ------- Principal -------
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+
+  // 4 - custom hook
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  // console.log(data);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // 1 - resgatando dados
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+    // console.log(product);
+
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",        
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+    // // 3 - carregamento dinâmico
+    // const addedProduct = await res.json();
+
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - refatorando post
+    httpConfig(product, "POST");
+
+
+    setName("");
+    setPrice("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!loading && <ul>
+        {items && items.map(product => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}     
+      </ul>}
+      <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+              Preço:
+              <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            {/* 7 - state de loading no post */  }
+            {loading && <input type="submit" disabled value="Aguarde" />}
+            {!loading && <input type="submit" value="Adicionar" />}
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
